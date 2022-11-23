96e17d5c6af900ce1f3fd446916493e501abbaee
naca-nyan <naca.nyan@gmail.com>
2022-11-24 01:46:24 +0900

    :sparkles: Add profile text

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -3,19 +3,35 @@ const Profile = () => {
     {
       name: "夜桜りん",
       pic: "/pic-lin.png",
-      detail: "りん".repeat(40),
+      detail: [
+        "ギター・ボーカル担当。バンドリーダー。",
+        "だいたいの楽曲の作詞をしている。",
+        "じつはギターを弾くのがあんまり得意じゃないらしく、",
+        "隙あらばサボろうとしているらしい。がぶがぶがぶりあす。",
+      ].join(""),
       twitter: "https://twitter.com/lin_ll_vr",
     },
     {
       name: "もものき もも",
       pic: "/pic-momonoki.png",
-      detail: "もちょ".repeat(26),
+      detail: [
+        "ドラム・ボーカル担当。バンドキャプテン。",
+        "いつも元気な性格やノリノリのビートだけでなく、",
+        "ロゴ・ポスターの作成や、バンドグッズの制作など、",
+        "ビジュアル・デザイン面でもみんなを引っ張っている。",
+        "もちょもちょ。",
+      ].join(""),
       twitter: "https://twitter.com/MoMoChannel9",
     },
     {
       name: "なかにゃん",
       pic: "/pic-nacanyan.png",
-      detail: "んな".repeat(40),
+      detail: [
+        "ベース・ボーカル担当。バンドマスター。",
+        "だいたいの楽曲の作曲をしている。いつも眠そうにしているが、",
+        "楽器を持つと目が覚めるらしい。",
+        "たまにキーボードやギターも担当する。なかんにゃ。",
+      ].join(""),
       twitter: "https://twitter.com/naca_nyan",
     },
   ];
@@ -44,7 +60,7 @@ const Profile = () => {
                   </h2>
                 </div>
                 <div class="flex md:block">
-                  <p class="w-3/4 md:(w-full font-bold)">{profile.detail}</p>
+                  <p class="w-3/4 md:w-full">{profile.detail}</p>
                   <div class="w-1/4 vh-center md:(mt-8)">
                     <a href={profile.twitter}>
                       <img
